cd0737f988ba743a092695bcda578d36f9fa3ee3
Your NameArniss5 <kat.dobr@gmail.com>
2023-01-17 18:12:22 +0000

    move data to a separate file

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
+import {services, articles} from "./data.js"
+
 const burgerIcon = document.querySelector(".burger-icon")
 const burgerNav = document.getElementById("burger-nav")
+const servicesContainer = document.getElementById("services-container")
+const articlesContainer = document.getElementById("articles-container")
+
+
 burgerIcon.addEventListener("click", toggleBurgerNav)
-
-// console.log(burgerIcon)
-
 
 
 function toggleBurgerNav() {
-    console.log("click")
     burgerIcon.classList.toggle("fa-bars")
     burgerIcon.classList.toggle("fa-xmark")
     burgerNav.classList.toggle("hamburger-nav")
 }
+
+let servicesEl = ""
+services.forEach(service => {
+    servicesEl += (
+        `<div class="service">
+            <img src="./images/${service.img}" alt="#" class="service-img">
+            <h3>${service.title}</h3>
+            <p>${service.text}</p>
+        </div>`
+    )
+})
+
+let articlesEl = ""
+articles.forEach(article => {
+    articlesEl += (
+        `<article>
+            <img src="./images/${article.img}" alt="banknotes">
+            <div class="article-text">
+                <div class="article-author">By ${article.author}</div>
+                <h4>${article.title}</h4>
+                <p>${article.text}</p> 
+                </div>
+        </article>`
+    )
+})
+
+servicesContainer.innerHTML = servicesEl
+articlesContainer.innerHTML = articlesEl
---
+++ data.js
@@ -0,0 +1,53 @@
+const articles = [
+    {
+        author: "Claire Robinson",
+        title: "Receive money in any currency with no fees",
+        text: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+        img: "image-currency.jpg"
+    },
+    {
+        author: "Wilson Hutton",
+        title: "Treat yourself without worrying about money",
+        text: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
+        img: "image-restaurant.jpg"
+    },
+    {
+        author: "Claire Robinson",
+        title: "Take your Easybank card wherever you go",
+        text: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
+        img: "image-plane.jpg"
+    },
+    {
+        author: "Wilson Hutton",
+        title: "Our invite-only Beta accounts are now live!",
+        text: "After a lot of hard work by the whole team, we're excited to launch our closed beta. It's easy to request an invite through the site ...",
+        img: "image-confetti.jpg"
+    }
+]
+
+
+const services = [
+    {
+        title: "Online Banking",
+        text: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+        img: "icon-online.svg"
+    },
+    {
+        title: "Simple Budgeting",
+        text: "See exactly where your money goes each month. Receive notifications when you're close to hitting your limits.",
+        img: "icon-budgeting.svg"
+    },
+    {
+        title: "Fast Onboarding",
+        text: "We don't do branches. Open your account in minutes online and start taking control of your finances right away.",
+        img: "icon-onboarding.svg"
+    },
+    {
+        title: "Open API",
+        text: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+        img: "icon-api.svg"
+    }
+
+]
+
+export {articles, services}
